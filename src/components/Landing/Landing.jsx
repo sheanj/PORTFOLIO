2a2c37e669a6e24359d0b83e8d80b8aa6e9fa370
Shean <csheanjohnson@gmail.com>
2020-07-30 14:53:53 -0400

    adjusted styling for mobile to display .about text-clip inline

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import "../Landing/Landing.css";
 import MyPortrait from "../../assets/me.jpeg";
 // import { Link } from "react-router-dom";
 import Card from "../Contact/Card";
+import ReactPageScroller from 'react-page-scroller';
 
 export default class Landing extends Component {
   render() {
@@ -11,25 +12,26 @@ export default class Landing extends Component {
         <header>
           <Card />
         </header>
-        <div className="landing">
+        <viewport className="landing">
           <section className="greeting">
-            <img src={MyPortrait} alt="pic" />
+            <img src={MyPortrait} alt="pic" className="fade-in"/>
             <div className="about fade-in">
-              <h1>
+              <span><h1>
                 Hello, World. <br /> My name is Shean && <br />
                 I'm a software engineer in Brooklyn.
               </h1>
+                <br />
               <h3>Scroll to see what I'm working on.</h3>
+              </span>
             </div>
           </section>
-          {/* <div className="project">
+          <div className="project1">
             <h1>CURA</h1>
           </div>
-          <div className="project">
+          <div className="project2">
           <h1>COVIDATA</h1>
-          </div> */}
-
-        </div>
+          </div>
+        </viewport>
       </>
     );
   }
