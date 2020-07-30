8140315b76a545dda5003dbe71fe35f9ebef68ff
Shean <csheanjohnson@gmail.com>
2020-07-30 17:14:14 -0400

    FA icon additions to header, mobile alignment adjustments

--- src/components/Contact/Card.jsx
+++ src/components/Contact/Card.jsx
@@ -1,28 +1,61 @@
 import React, { Component } from "react";
 import "./Card.css";
+import {
+  FaTwitter,
+  FaEnvelope,
+  FaPhone,
+  FaGit,
+  FaGithub,
+  FaLinkedin,
+  FaPage4,
+  FaPagelines,
+  FaHandPaper,
+  FaPaperclip,
+} from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export default class Card extends Component {
   render() {
     return (
       <div className="contact">
-        <a href="mailto:csheanjohnson@gmail.com">
-          <h4>[email]</h4>
+        <IconContext.Provider
+          value={{
+            style: {
+              fontSize: "25px",
+              color: "#ffffff",
+              paddingLeft: "8px",
+              paddingTop: "5px",
+            },
+          }}
+        >
+          <a href="mailto:csheanjohnson@gmail.com">
+            <FaEnvelope />
+          </a>
+          <a href="tel:+19174742061">
+            <FaPhone />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/sheanj"
+          >
+            <FaGithub />
+          </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/sheanj/"
+        >
+            <FaLinkedin />
         </a>
-        <a href="tel:+19174742061">
-          <h4>[phone]</h4>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://docdro.id/YbJTilE"
+        >
+            <FaPaperclip />
         </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/sheanj">
-          <h4>[github]</h4>
-        </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sheanj/">
-          <h4>[linkedin]</h4>
-        </a>
-        <a target="_blank" rel="noopener noreferrer" href="https://docdro.id/YbJTilE">
-          <h4>[resume]</h4>
-        </a>
-        {/* <a target="_blank" href="https://docdro.id/YbJTilE">
-          <h4>[view my work]</h4>
-        </a> */}
+          </IconContext.Provider>
       </div>
     );
   }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,9 +3,8 @@ import "../Landing/Landing.css";
 import MyPortrait from "../../assets/me.jpeg";
 // import { Link } from "react-router-dom";
 import Card from "../Contact/Card";
-import FadeIn from 'react-fade-in';
-import Carrat from '../../assets/carrat.jsx'
-
+import FadeIn from "react-fade-in";
+import Carrat from "../../assets/carrat.jsx";
 
 export default class Landing extends Component {
   render() {
@@ -15,26 +14,28 @@ export default class Landing extends Component {
           <Card />
         </header>
         <FadeIn>
-        <viewport className="landing">
-          <section className="greeting">
-            <img src={MyPortrait} alt="pic" className="fade-in"/>
-            <div className="about fade-in">
-              <span><h1>
-                Hello, World. <br /> My name is Shean && <br />
-                I'm a software engineer in Brooklyn.
-              </h1>
-                <br />
-                <br />
+          <viewport className="landing">
+            <section className="greeting">
+              <img src={MyPortrait} alt="pic" className="fade-in" />
+              <div className="about fade-in">
+                <span>
+                  <h1>
+                    Hello, World. <br /> {"{"} My name is Shean && <br />
+                    I'm a software engineer in Brooklyn. {"}"}
+                  </h1>
+                  <br />
+                  <h3>Scroll to view what I'm working on</h3>
+                  <br />
                   <Carrat />
-              </span>
+                </span>
+              </div>
+            </section>
+            <div className="project1">
+              <h1>CURA</h1>
             </div>
-          </section>
-          <div className="project1">
-            <h1>CURA</h1>
-          </div>
-          <div className="project2">
-          <h1>COVIDATA</h1>
-          </div>
+            <div className="project2">
+              <h1>COVIDATA</h1>
+            </div>
           </viewport>
         </FadeIn>
       </>
